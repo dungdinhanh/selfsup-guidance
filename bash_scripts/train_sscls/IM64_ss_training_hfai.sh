531353e641a35ed7d08212847b19ed2ca68a4a3d
dung <dad@abc.c>
2023-08-17 13:31:13 +0000

    training selfsup scratch complete

--- bash_scripts/train_sscls/IM64_ss_training_hfai.sh
+++ bash_scripts/train_sscls/IM64_ss_training_hfai.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TRAIN_FLAGS="--iterations 300000 --anneal_lr True --batch_size 128 --lr 6e-4 --save_interval 10000 --weight_decay 0.2"
+export NCCL_P2P_DISABLE=1
+
+TRAIN_FLAGS="--iterations 300000 --anneal_lr True --batch_size 60 --lr 6e-4 --save_interval 10000 --weight_decay 0.2"
 CLASSIFIER_FLAGS="--image_size 64 --classifier_attention_resolutions 32,16,8 --classifier_depth 4 \
 --classifier_width 128 --classifier_pool attention --classifier_resblock_updown True \
  --classifier_use_scale_shift_norm True"
@@ -14,6 +16,6 @@ echo ${cmd}
 eval ${cmd}
 
 
-cmd="python scripts_gdiff/selfsup/classifier_train_selfsup.py --data_dir path/to/imagenet --logdir runs/classifier_training/ $TRAIN_FLAGS $CLASSIFIER_FLAGS"
+cmd="python scripts_gdiff/selfsup/classifier_train_selfsup.py --data_dir path/to/imagenet --logdir runs/selfsup_training/scratch $TRAIN_FLAGS $CLASSIFIER_FLAGS"
 echo ${cmd}
 eval ${cmd}
