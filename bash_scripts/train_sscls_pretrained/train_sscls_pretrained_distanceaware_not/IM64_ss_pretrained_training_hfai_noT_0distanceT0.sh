1d45764bee33ddbb221e1c960cfe32f033b30f67
dung <dad@abc.c>
2023-08-27 14:38:13 +0000

    save change

---
+++ bash_scripts/train_sscls_pretrained/train_sscls_pretrained_distanceaware_not/IM64_ss_pretrained_training_hfai_noT_0distanceT0.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export NCCL_P2P_DISABLE=1
+
+iter="50000"
+
+TRAIN_FLAGS="--iterations ${iter} --anneal_lr True --batch_size 60 --lr 6e-4 --save_interval 10000 --weight_decay 0.2 \
+--pretrained_cls models/64x64_classifier.pt"
+CLASSIFIER_FLAGS="--image_size 64 --dim 2048 --pred_dim 512"
+
+cmd="cd .."
+#echo ${cmd}
+#eval ${cmd}
+
+cmd="ls"
+echo ${cmd}
+eval ${cmd}
+
+
+cmd="python scripts_gdiff/selfsup/classifier_train_selfsup_zerodistancet0_not_simsiam.py --data_dir path/to/imagenet --logdir \
+runs/selfsup_training_distanceaware_noT_0distanceT0/pretrainedcls${iter} $TRAIN_FLAGS $CLASSIFIER_FLAGS"
+echo ${cmd}
+eval ${cmd}
