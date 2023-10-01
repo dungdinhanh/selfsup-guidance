edbbe5e7a3b8fe040e7715f5a9582b70df573d26
Dzung <abc@xyz>
2023-10-01 23:52:18 +0000

    save change

---
+++ bash_scripts/runs_bash/run3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#CUDA_VISIBLE_DEVICES=0,1,2,3 hfai bash bash_scripts/sample_selfsup/im64cond/IM64_cond_ss_sampling_hfai_negw_maxp700_2.sh ++
+
+hfai bash bash_scripts/sample_selfsup/im64unc/IM64_unc_ss_sampling_hfai_negw_maxp.sh ++
+
+
+
+
+
---
+++ bash_scripts/sample_selfsup/im128cond_psimsiam/IM128_cond_ss_sampling_hfai_negw_maxp700_wl.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+SAMPLE_FLAGS="--batch_size 100 --num_samples 50000 --timestep_respacing 250"
+#SAMPLE_FLAGS="--batch_size 32 --num_samples 50000 --timestep_respacing 250"
+#TRAIN_FLAGS="--lr 1e-4 --batch_size 128 --schedule_sampler loss-second-moment"
+export NCCL_P2P_DISABLE=1
+MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond True --diffusion_steps 1000 --image_size 128 \
+--learn_sigma True --noise_schedule linear --num_channels 256 --num_heads 4 --num_res_blocks 2 --resblock_updown True\
+ --use_fp16 True --use_scale_shift_norm True"
+
+
+#MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond True --diffusion_steps 1000 --dropout 0.1 --image_size 64 \
+# --learn_sigma True --noise_schedule cosine --num_channels 192 --num_head_channels 64 --num_res_blocks 3 \
+#  --resblock_updown True --use_new_attention_order True --use_fp16 True --use_scale_shift_norm True"
+cmd="cd ../../../"
+#echo ${cmd}
+#eval ${cmd}
+
+cmd="ls"
+echo ${cmd}
+eval ${cmd}
+
+scales=("0.5" "1.0" "2.0" )
+
+
+for scale in "${scales[@]}"
+do
+cmd="python scripts_gdiff/selfsup/classifier_sample_ss_psimsiam.py $MODEL_FLAGS --classifier_scale ${scale}  \
+--classifier_path runs/selfsup_training_distanceaware_noT/psimsiam300000_IM256_wl_maxp700/models/model150000.pt \
+ --model_path models/128x128_diffusion.pt $SAMPLE_FLAGS \
+ --logdir runs/sampling_ss/IMN128/conditional/scale${scale}_wl_maxp700/ "
+echo ${cmd}
+eval ${cmd}
+done
+
+for scale in "${scales[@]}"
+do
+cmd="python evaluations/evaluator_tolog.py reference/VIRTUAL_imagenet128_labeled.npz \
+ runs/sampling_ss/IMN128/conditional/scale${scale}_wl_maxp700/reference/samples_50000x128x128x3.npz"
+echo ${cmd}
+eval ${cmd}
+done
+
+
+
+
+#cmd="python scripts_hfai_gdiff/classifier_sample.py --logdir runs/sampling/IMN64/conditional/ \
+# ${MODEL_FLAGS} --classifier_scale 1.0 --classifier_path models/64x64_classifier.pt \
+# --classifier_depth 4 --model_path models/64x64_diffusion.pt ${SAMPLE_FLAGS}"
+#echo ${cmd}
+#eval ${cmd}
