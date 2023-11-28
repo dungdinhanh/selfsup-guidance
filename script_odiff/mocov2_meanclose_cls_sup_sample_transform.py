64073ab62c55dfdec88a5f79b91aae5a752b23be
Dzung <abc@xyz>
2023-11-28 04:47:51 +0000

    save change

--- script_odiff/mocov2_meanclose_cls_sup_sample_transform.py
+++ script_odiff/mocov2_meanclose_cls_sup_sample_transform.py
@@ -251,6 +251,10 @@ def main(local_rank):
         random_selected_indexes = np.random.randint(0, features_n, (n,), dtype=int)
         p_features = th.from_numpy(features_p[random_selected_indexes]).to(dist_util.dev())
         classes = th.from_numpy(labels_associated[random_selected_indexes]).to(dist_util.dev())
+        mask_features = th.zeros_like(p_features)
+        mask_features[classes] = 1.0 * (1/args.k_closest)
+        
+        print(mask_features)
 
         model_kwargs["y"] = classes
         model_kwargs["p_features"] = p_features
