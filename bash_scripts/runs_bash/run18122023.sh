b08506eea7711a6a721b36d2cde28486e80171ca
Dzung <abc@xyz>
2024-01-06 19:16:43 +0000

    save change

--- bash_scripts/runs_bash/run18122023.sh
+++ bash_scripts/runs_bash/run18122023.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-hfai bash bash_scripts/sample_offtheshelf/im64cond_ots_mocov2_contrastive/IM64_cond_ots_mocov2_contrastive_instancebased.sh ++
+#hfai bash bash_scripts/sample_offtheshelf/im64cond_ots_mocov2_contrastive/IM64_cond_ots_mocov2_contrastive_instancebased.sh ++
+#
+#hfai bash bash_scripts/sample_offtheshelf/im64cond_ots_mocov2_contrastive/IM64_cond_ots_mocov2_smoothcontrastive_instancebased.sh ++
+#
+#hfai bash bash_scripts/sample_offtheshelf/im64cond_ots_mocov2_contrastive/IM64_cond_ots_mocov2_contrastive_instancebased3.sh ++
 
-hfai bash bash_scripts/sample_offtheshelf/im64cond_ots_mocov2_contrastive/IM64_cond_ots_mocov2_smoothcontrastive_instancebased.sh ++
-
-hfai bash bash_scripts/sample_offtheshelf/im64cond_ots_mocov2_contrastive/IM64_cond_ots_mocov2_contrastive_instancebased3.sh ++
+hfai bash bash_scripts/sample_offtheshelf/im64cond_ots_mocov2_contrastive/IM64_cond_ots_mocov2_smoothcontrastive_instancebased3.sh ++
