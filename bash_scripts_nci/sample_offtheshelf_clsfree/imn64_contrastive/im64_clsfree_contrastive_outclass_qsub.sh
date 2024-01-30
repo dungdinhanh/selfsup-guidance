134f15b33cf23c69b30ffa64de8a9ae7f3ab69f6
Dung Dinh <dd9648@gadi-login-09.gadi.nci.org.au>
2024-01-31 06:54:13 +1100

    save change

--- bash_scripts_nci/sample_offtheshelf_clsfree/imn64_contrastive/im64_clsfree_contrastive_outclass_qsub.sh
+++ bash_scripts_nci/sample_offtheshelf_clsfree/imn64_contrastive/im64_clsfree_contrastive_outclass_qsub.sh
@@ -4,10 +4,10 @@ cscales=("10.0" "12.0" "14.0" "16.0" "18.0")
 
 for cscale in "${cscales[@]}"
 do
-  cmd="qsub bash_scripts_nci/sample_offtheshelf_clsfree/imn64_contrastive/im64_clsfree_contrastive_outclass_tunec.sh \
-   ${cscale} -o output_nci/log_clsfree_contrastive_tunec${cscale}.txt -e output_nci/error_clsfree_contrastive_tunec${cscale}.txt"
+  cmd="qsub -v cscale='${cscale}' bash_scripts_nci/sample_offtheshelf_clsfree/imn64_contrastive/im64_clsfree_contrastive_outclass_tunec.sh \
+   -o output_nci/log_clsfree_contrastive_tunec${cscale}.txt  -e output_nci/error_clsfree_contrastive_tunec${cscale}.txt "
    echo ${cmd}
-#   eval ${cmd}
+   eval ${cmd}
    done
 #PBS -o output_nci/log5.txt
 #PBS -e output_nci/error5.txt
